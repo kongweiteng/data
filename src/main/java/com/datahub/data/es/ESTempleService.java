18b335cde4e9dd7d439ab71025fa690d3b8e7bae
kongweiteng <kwt19921992>
2019-04-09 16:16:09 +0800

    es

---
+++ src/main/java/com/datahub/data/es/ESTempleService.java
@@ -0,0 +1,36 @@
+package com.datahub.data.es;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class ESTempleService {
+
+    @Resource
+    ElasticsearchTemplate elasticsearchTemplate;
+
+    public JSONObject get() {
+//        SumBuilder sb = AggregationBuilders.sum("tpPrice").field("payPrice");
+//        BoolQueryBuilder bqb = QueryBuilders.boolQuery();
+//        bqb.must(QueryBuilders.termQuery("state",SuperAppConstant.PAY_STATUS_SUCCESS));
+//        bqb.must(QueryBuilders.termQuery("appId",appId));
+//        bqb.must(QueryBuilders.boolQuery()
+//                .should(QueryBuilders.matchQuery("payPlatform", SuperAppConstant.PAY_PLATFORM_TL))
+//                .should(QueryBuilders.matchQuery("payPlatform", SuperAppConstant.PAY_PLATFORM_TL_WX_APP))
+//                .should(QueryBuilders.matchQuery("payPlatform", SuperAppConstant.PAY_PLATFORM_TL_ALI))
+//                .should(QueryBuilders.matchQuery("payPlatform", SuperAppConstant.PAY_PLATFORM_TL_WX_JS))
+//                .should(QueryBuilders.matchQuery("payPlatform", SuperAppConstant.PAY_PLATFORM_TL_APP))
+//                .should(QueryBuilders.matchQuery("payPlatform", SuperAppConstant.PAY_PLATFORM_TL_WX_H5))
+//                .should(QueryBuilders.matchQuery("payPlatform", SuperAppConstant.PAY_PLATFORM_WX_GWORLD))
+//                .should(QueryBuilders.matchQuery("payPlatform", SuperAppConstant.PAY_PLATFORM_ALI_GWORLD))
+//        );
+//        SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(bqb).withIndices(SuperAppConstant.ES_INDEX_NAME).withTypes(SuperAppConstant.ES_PAY_TYPE)
+//                .withSearchType(SearchType.DEFAULT)
+//                .addAggregation(sb).build();
+        return null;
+    }
+
+}

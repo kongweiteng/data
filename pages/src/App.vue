6a546cc70ff85135afee9d2bd16b6a40aa5ce6e0
kongweiteng <kongweiteng@enn.cn>
2019-04-19 09:37:54 +0800

    使用vue admin 框架

--- pages/src/App.vue
+++ pages/src/App.vue
@@ -1,35 +1,11 @@
 <template>
-    <div id="app">
-        <left></left>
-        <router-view>
-        </router-view>
-        <foot></foot>
-    </div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-    // import HelloWorld from './components/HelloWorld.vue'
-
-    import foot from '@/components/foot/foot.vue'
-    import left from '@/components/left/left.vue'
-
-    export default {
-        name: 'app',
-        components: {
-            // HelloWorld
-            foot,
-            left
-        }
-    }
+export default {
+  name: 'App'
+}
 </script>
-
-<style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>

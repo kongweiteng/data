9d4f00d8e5c3d7ab9baf692473af22c2850fb40f
kongweiteng <kongweiteng@enn.cn>
2019-04-28 09:30:45 +0800

    爬虫

--- src/main/java/com/datahub/data/DataApplication.java
+++ src/main/java/com/datahub/data/DataApplication.java
@@ -3,9 +3,11 @@ package com.datahub.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"com.datahub.data.feign"})
+@EnableScheduling
 public class DataApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/datahub/data/job/TestJobs.java
@@ -0,0 +1,24 @@
+package com.datahub.data.job;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Slf4j
+public class TestJobs {
+
+    @Scheduled(cron = "0/10 * *  * * ? ")   //每10秒执行一次
+    public void test() {
+        try {
+            log.info("当前时间：" + System.currentTimeMillis());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
